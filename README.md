97607c381b79930469bd2ef01d55fd4ed3cf9c6a
Gabe <gabrieltucker260@gmail.com>
2023-11-02 10:06:35 -0700

    fixed bugs, working on hiding name and buttons
    Co-authored-by Teddy Peterschmidt <t.a.peterschmidt@gmail.com>

---
+++ README.md
@@ -0,0 +1,57 @@
+# _Pig Dice_
+
+#### By _**Gabriel Tucker & Teddy Peterschmidt**_
+
+#### _Pig Dice Game_
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _JavaScript_
+
+## Description
+
+_a program where two users can play Pig dice against each other. "_
+
+## Github Page Link
+
+https://p2nts.github.io/diceGame/
+
+## Setup/Installation Requirements
+* _DownLoad Live Server Extension for VsCode_
+* _Make Sure to have the Live server extension for VSCode_
+* _Click "Go Live"  On Bottom Right Or Right Click And press "Open With Live Server On Index.html_
+* _Or Click GhPages Link Above _
+
+
+
+
+## Known Bugs
+
+* _None as of now._
+
+
+## License
+
+_The MIT License (MIT)_
+
+Copyright (c) _2023_ _Gabriel Tucker_
+
+## Test
+
+Test: Create random number generator
+Code: 
+Expected Output: 
+
+Test: if a "1" is rolled the total is set to zero.
+Code: 
+Expected Output: 
+
+Test: add score total to a overall score value 
+Code: 
+Expected Output: 
+
+Test: if overall score is 100 game ends 
+Code: 
+Expected Output: 
---
+++ js/scripts.js
@@ -0,0 +1,137 @@
+//Business Logic
+function PlayerCreate(playerName, turnScore, overallScore) {
+    this.playerName = playerName,
+    this.turnScore = turnScore;
+    this.overallScore = overallScore;
+}
+
+function diceRoll() {
+    const randomInt = Math.floor(Math.random() * 6) + 1;
+    return randomInt;
+}
+
+function getTurnScore() {
+    const roll = diceRoll();
+    let turnScore = 0;
+    if (roll >= 2) {
+        turnScore += roll;
+    }
+    return turnScore
+}
+
+PlayerCreate.prototype.addTurnScore = function () {
+    const turnScoreValue = getTurnScore();
+    if (turnScoreValue === 0) {
+        this.turnScore = 0;
+    } else { this.turnScore += turnScoreValue };
+}
+
+PlayerCreate.prototype.addOverallScore = function () {
+    this.overallScore += this.turnScore;
+    this.turnScore = 0;
+}
+
+
+//UI Logic
+
+let player1 = new PlayerCreate("", 0, 0);
+let player2 = new PlayerCreate("", 0, 0);
+
+function handleNameForm() {
+    event.preventDefault();
+    const p1Name = document.querySelector("input#p1Name").value;
+    const p2Name = document.querySelector("input#p2Name").value;
+    const p1Header = document.querySelector("h2#firstPlayer");
+    const p2Header = document.querySelector("h2#secondPlayer");
+    const p1Score = document.querySelector("span#firstPlayerScore");
+    const p2Score = document.querySelector("span#secondPlayerScore");
+   
+    player1.playerName = p1Name;
+    player2.playerName = p2Name;
+
+    p1Header.innerText = player1.playerName;
+    p2Header.innerText = player2.playerName;
+    p2Score.innerText = player2.playerName + ": ";
+    p1Score.innerText = player1.playerName + ": ";
+
+    // document.querySelector("div#player1").removeAttribute("class", "hidden");
+    // document.querySelector("div#scores").removeAttribute("class", "hidden");
+    // document.querySelector("form#playerNameForm").setAttribute("id", "hidden");
+}
+
+function endTurn1() {
+    player1.addOverallScore();
+    const player1Score = document.querySelector("span#p1Score");
+    const h2 = document.createElement("h2");
+    h2.append(player1.overallScore);
+    player1Score.innerText = h2.textContent;
+    if (player1.overallScore >= 100) {
+        document.querySelector("h1#player1Wins").removeAttribute("class", "hidden");
+        document.querySelector("div#scores").setAttribute("class", "hidden");
+    }
+    document.querySelector("#p2TurnScore").innerText = "0"
+    document.querySelector("div#player2").removeAttribute("class", "hidden");
+    document.querySelector("div#player1").setAttribute("class", "hidden");
+}
+
+function endTurn2() {
+    player2.addOverallScore();
+    const player2Score = document.querySelector("span#p2Score");
+    const h2 = document.createElement("h2");
+    h2.append(player2.overallScore);
+    player2Score.innerText = h2.textContent;
+    if (player2.overallScore >= 100) {
+        document.querySelector("h1#player2Wins").removeAttribute("class", "hidden");
+        document.querySelector("div#scores").setAttribute("class", "hidden");
+    }
+    document.querySelector("#p1TurnScore").innerText = "0"
+    document.querySelector("div#player1").removeAttribute("class", "hidden");
+    document.querySelector("div#player2").setAttribute("class", "hidden");
+}
+
+function rollDice1() {
+    player1.addTurnScore()
+    console.log(player1.turnScore);
+    const player1Turn = document.querySelector("#p1TurnScore");
+    player1Turn.innerText = player1.turnScore;
+
+    if (player1.turnScore === 0) {
+        document.querySelector("#p2TurnScore").innerText = "0"
+        document.querySelector("div#player2").removeAttribute("class", "hidden");
+        document.querySelector("div#player1").setAttribute("class", "hidden");
+    }
+
+    return player1.turnScore;
+}
+
+function rollDice2() {
+    player2.addTurnScore();
+    console.log(player2.turnScore)
+    const player2Turn = document.querySelector("#p2TurnScore");
+    player2Turn.innerText = player2.turnScore;
+
+    if (player2.turnScore === 0) {
+        document.querySelector("#p2TurnScore").innerText = "0";
+        document.querySelector("div#player1").removeAttribute("class", "hidden");
+        document.querySelector("div#player2").setAttribute("class", "hidden");
+    }
+
+    return player2.turnScore;
+}
+
+window.addEventListener("load", function () {
+    document.querySelector("form#playerNameForm").addEventListener("submit", handleNameForm);
+
+    document.querySelector("button#p1Roll").addEventListener("click", rollDice1);
+    document.querySelector("button#p2Roll").addEventListener("click", rollDice2);
+    document.querySelector("button#p1End").addEventListener("click", endTurn1);
+    document.querySelector("button#p2End").addEventListener("click", endTurn2);
+
+    document.querySelector("h1#player1Wins").setAttribute("class", "hidden");
+    document.querySelector("h1#player2Wins").setAttribute("class", "hidden");
+    
+    // document.querySelector("div#player1").setAttribute("class", "hidden");
+    document.querySelector("div#player2").setAttribute("class", "hidden");
+    // document.querySelector("div#scores").setAttribute("class", "hidden");
+});
+
