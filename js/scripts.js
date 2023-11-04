fb2244b6c436712cc3d73ce951aaabc76a22428b
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2023-11-03 18:03:50 -0700

    Add a display of the total price if multiple pizzas are ordered.

--- js/scripts.js
+++ js/scripts.js
@@ -67,14 +67,14 @@ function soloMode() {
     event.preventDefault();
     const p2Input = document.querySelector("div#p2Input");
     p2Input.remove();
-    
+
 }
 
 function handleNameForm() {
     event.preventDefault();
 
-    const p2Input = document.querySelector("div#p2Input");
-    const playerOption = document.querySelector("select#selectOption").value;
+    // const p2Input = document.querySelector("div#p2Input");
+    // const playerOption = document.querySelector("select#selectOption").value;
     const p1Name = document.querySelector("input#p1Name").value;
     const p1Header = document.querySelector("h2#firstPlayer");
     const p2Header = document.querySelector("h2#secondPlayer");
@@ -83,32 +83,32 @@ function handleNameForm() {
     const p1Wins = document.querySelector("h1#player1Wins");
     const p2Wins = document.querySelector("h1#player2Wins");
 
-    document.querySelector("form#selectMenu").setAttribute("class", "hidden");
+    // document.querySelector("form#selectMenu").setAttribute("class", "hidden");
     document.querySelector("div#player1").removeAttribute("class", "hidden");
     document.querySelector("div#scores").removeAttribute("class", "hidden");
     document.querySelector("form#playerNameForm").setAttribute("class", "hidden");
 
-    if (playerOption === "solo") {
-        player1.playerName = p1Name;
-        p1Header.innerText = player1.playerName;
-        p2Header.innerText = "Computer";
-        p1Score.innerText = player1.playerName + ": ";
-        p2Score.innerText = "Computer: ";
-        p1Wins.innerText = player1.playerName + " wins!";
-        p2Wins.innerText = "Computer wins!";
-    } else {
-        const p2Name = document.querySelector("input#p2Name").value;
+    // if (playerOption === "solo") {
+    //     player1.playerName = p1Name;
+    //     p1Header.innerText = player1.playerName;
+    //     p2Header.innerText = "Computer";
+    //     p1Score.innerText = player1.playerName + ": ";
+    //     p2Score.innerText = "Computer: ";
+    //     p1Wins.innerText = player1.playerName + " wins!";
+    //     p2Wins.innerText = "Computer wins!";
+    // } else {
+    const p2Name = document.querySelector("input#p2Name").value;
 
-        player1.playerName = p1Name;
-        player2.playerName = p2Name;
-        p1Header.innerText = player1.playerName;
-        p2Header.innerText = player2.playerName;
-        p1Score.innerText = player1.playerName + ": ";
-        p2Score.innerText = player2.playerName + ": ";
-        p1Wins.innerText = player1.playerName + " wins!";
-        p2Wins.innerText = player2.playerName + " wins!";
-    }
+    player1.playerName = p1Name;
+    player2.playerName = p2Name;
+    p1Header.innerText = player1.playerName;
+    p2Header.innerText = player2.playerName;
+    p1Score.innerText = player1.playerName + ": ";
+    p2Score.innerText = player2.playerName + ": ";
+    p1Wins.innerText = player1.playerName + " wins!";
+    p2Wins.innerText = player2.playerName + " wins!";
 }
+// }
 
 function endTurn1() {
     player1.addOverallScore();
@@ -120,18 +120,18 @@ function endTurn1() {
         winner1();
     }
 
-    if (playerOption === "solo") {
-        rollDice2(); // Initial dice roll
-        const rollUntil12OrGreater = () => {
-            if (player2.turnScore < 12) {
-                rollDice2(); // Roll the dice
-                setTimeout(rollUntil12OrGreater, 1000); // Repeat after 1 second (adjust time as needed)
-            }
-        };
-        rollUntil12OrGreater(); // Start the rolling loop
-    }
-    
+    // if (playerOption === "solo") {
+    //     rollDice2(); // Initial dice roll
+    //     const rollUntil12OrGreater = () => {
+    //         if (player2.turnScore < 12) {
+    //             rollDice2(); // Roll the dice
+    //             setTimeout(rollUntil12OrGreater, 1000); // Repeat after 1 second (adjust time as needed)
+    //         }
+    //     };
+    //     rollUntil12OrGreater(); // Start the rolling loop
+    // }
 
+    document.querySelector("#p2NumberRolled").innerText = "";
     document.querySelector("#p2TurnScore").innerText = "0"
     document.querySelector("div#player2").removeAttribute("class", "hidden");
     document.querySelector("div#player1").setAttribute("class", "hidden");
@@ -146,6 +146,7 @@ function endTurn2() {
     if (player2.overallScore >= 100) {
         winner2();
     }
+    document.querySelector("#p1NumberRolled").innerText = "";
     document.querySelector("#p1TurnScore").innerText = "0"
     document.querySelector("div#player1").removeAttribute("class", "hidden");
     document.querySelector("div#player2").setAttribute("class", "hidden");
@@ -163,7 +164,7 @@ function rollDice1() {
         document.querySelector("#p2TurnScore").innerText = "0";
         document.querySelector("div#player2").removeAttribute("class", "hidden");
         document.querySelector("div#player1").setAttribute("class", "hidden");
-        rollDice2();
+        // rollDice2();
     }
     if ((player1.overallScore + player1.turnScore) >= 100) {
         winner1();
@@ -172,7 +173,7 @@ function rollDice1() {
 }
 
 function rollDice2() {
-    const playerOption = document.querySelector("select#selectOption").value;
+    // const playerOption = document.querySelector("select#selectOption").value;
     const player2Turn = document.querySelector("#p2TurnScore");
     const player2Roll = document.querySelector("#p2NumberRolled");
 
@@ -200,7 +201,7 @@ function rollDice2() {
 window.addEventListener("load", function () {
     document.querySelector("form#playerNameForm").addEventListener("submit", handleNameForm);
 
-    document.querySelector("button#modeButton").addEventListener("click", soloMode);
+    // document.querySelector("button#modeButton").addEventListener("click", soloMode);
     document.querySelector("button#p1Roll").addEventListener("click", rollDice1);
     document.querySelector("button#p2Roll").addEventListener("click", rollDice2);
     document.querySelector("button#p1End").addEventListener("click", endTurn1);
@@ -212,7 +213,5 @@ window.addEventListener("load", function () {
     document.querySelector("div#player1").setAttribute("class", "hidden");
     document.querySelector("div#player2").setAttribute("class", "hidden");
     document.querySelector("div#scores").setAttribute("class", "hidden");
-
-    const playOption = document.querySelector("select#selectOption").value;
 
 });
